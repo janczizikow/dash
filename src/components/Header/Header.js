8a1308a8ab9988895103fbbf27fd8303b19ce043
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-11 20:21:38 +0200

    Styling, adding react-icons & axios and env variables

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md';
+
 import { Container, Row } from '../Layout';
 
 import './Header.css';
 
 const Header = () => (
-  <header className="header">
-    <Container>
-      <Row gutters={false} middle between style={{ height: '3.625rem' }}>
+  <header className="header header--fixed">
+    <Container fluid>
+      <Row gutters={false} middle between className="header__inner">
         <NavLink to="/" className="header__logo">
           Logo
         </NavLink>
+        <button type="button" className="header__toggle">
+          <MdMenu size={24} style={{ verticalAlign: 'middle' }} />
+        </button>
         <div className="header__links">
-          <NavLink to="/" className="header__link-item">
+          <NavLink
+            exact
+            to="/"
+            className="header__link-item"
+            activeClassName="header__link-item--active"
+          >
             Home
           </NavLink>
-          <NavLink to="/" className="header__link-item">
-            Home
+          <NavLink
+            to="/login"
+            className="header__link-item"
+            activeClassName="header__link-item--active"
+          >
+            Log in
+          </NavLink>
+          <NavLink
+            to="/signup"
+            className="header__link-item"
+            activeClassName="header__link-item--active"
+          >
+            Sign up
           </NavLink>
         </div>
       </Row>
