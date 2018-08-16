55a7180e13e20b0e8a99741c42944deea2d14d73
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-16 10:25:45 +0200

    Add Label Component

---
+++ src/__tests__/Label.spec.js
@@ -0,0 +1,35 @@
+import Label from '../components/Label';
+
+describe('<Label />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Label htmlFor="input" />);
+  });
+  it('should render without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+  it('should render children', () => {
+    wrapper.setProps({ children: 'child' });
+    expect(wrapper.text()).toBe('child');
+  });
+
+  it('should pass htmlFor prop', () => {
+    expect(wrapper.html()).toBe('<label for="input" class="Label"></label>');
+  });
+
+  it('should add additional classnames when given a prop', () => {
+    wrapper.setProps({ className: 'extra' });
+    expect(wrapper.hasClass('extra')).toBeTruthy();
+  });
+
+  it('should add block class when given a prop', () => {
+    wrapper.setProps({ block: true });
+    expect(wrapper.hasClass('Label--block')).toBeTruthy();
+  });
+
+  it('should add invalid class when given a prop', () => {
+    wrapper.setProps({ invalid: true });
+    expect(wrapper.hasClass('Label--error')).toBeTruthy();
+  });
+});
---
+++ src/components/Label/Label.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './Label.css';
+
+const propTypes = {
+  htmlFor: PropTypes.string.isRequired,
+  block: PropTypes.bool,
+  bold: PropTypes.bool,
+  hidden: PropTypes.bool,
+  invalid: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.func,
+    PropTypes.string,
+  ]),
+};
+
+/* https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md */
+/* eslint-disable jsx-a11y/label-has-for */
+
+const Label = props => {
+  const {
+    htmlFor,
+    block,
+    hidden,
+    bold,
+    invalid,
+    className,
+    ...attributes
+  } = props;
+  const classes = cx(
+    'Label',
+    block && 'Label--block',
+    hidden && 'Label--hidden',
+    bold && 'Label--bold',
+    invalid && 'Label--error',
+    className
+  );
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={classes}
+      hidden={hidden}
+      {...attributes}
+    />
+  );
+};
+
+Label.propTypes = propTypes;
+
+export default Label;
